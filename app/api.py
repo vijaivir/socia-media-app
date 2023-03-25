75ccce557474e475bbc2e5fac1773c71458967e9
Vijaivir Dhaliwal <vijaivirdhaliwal@gmail.com>
2023-03-25 15:30:45 -0700

    api

---
+++ app/api.py
@@ -0,0 +1,78 @@
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+import time 
+import requests
+
+client = MongoClient()
+
+db = client.user_database
+table = db.user_table
+
+post_id = 1
+comment_id = 1
+
+app = Flask(__name__)
+
+"""
+{
+    "username":"",
+    "email":"",
+    "password":"",
+    "dob":"",
+    "friends": [{"username":""}],
+    "posts": [{"postid":"", "post":"", "likes":}],
+    "comments": [{"commentid":"", "postid":"", "comment":""}],
+    ],
+}
+"""
+
+@app.route('/add', methods=['POST'])
+def add():
+    data = request.json
+    table.insert_one({
+        'username':data['username'],
+        'email':data['email'],
+        'password':data['password'],
+        'dob':data['dob'],
+        'friends': [],
+        'posts': [],
+        'comments': []
+    })
+    return "Created New Account"
+
+@app.route('/create_post', methods=['POST'])
+def create_post():
+    global post_id
+    data = request.json
+    table.update_one({'username': data['username']}, {'$push': {'posts': {'post_id':post_id, 'post':data['post'], 'likes':0}}})
+    post_id += 1
+    return "Created Post"
+
+@app.route('/edit_post', methods=['POST'])
+def edit_post():
+    data = request.json
+    table.update_one({'username': data['username'], 'posts.post_id': int(data['post_id'])}, {'$set': {'posts.$.post': data['post']}})
+    return 'Updated Post'
+
+@app.route('/delete_post', methods=['POST'])
+def delete_post():
+    data = request.json
+    table.update_one({'username': data['username']}, {'$pull': {'posts': {'post_id': int(data['post_id'])}}})
+    return 'Deleted Post'
+
+@app.route('/comment', methods=['POST'])
+def comment():
+    global comment_id
+    data = request.json
+    table.update_one({'username': data['username']}, {'$push': {'comments': {'comment_id':comment_id, 'post_id':int(data['post_id']), 'comment':data['comment']}}})
+
+
+@app.route("/clear", methods=["GET"])
+def clear():
+    table.delete_many({})
+    return "cleared DB"
+    
+
+if __name__ =="__main__":
+    app.run(host="0.0.0.0", debug=True, port=5000)
+
---
+++ app/input.py
@@ -0,0 +1,35 @@
+import requests
+import sys
+import time
+
+apiUrl = "http://localhost:5000"
+
+def createCommand(line):
+    c = line.split(',')
+    cmd = c[0].split(" ")[1]
+    print(cmd)
+
+    if(cmd == 'ADD'):
+        requests.post(apiUrl + '/add', json={'cmd':cmd, 'username':c[1], 'dob':c[2], 'email':c[3], 'password':c[4]})
+    elif(cmd == 'CREATE_POST'):
+        requests.post(apiUrl + '/create_post', json= {'cmd':cmd, 'username':c[1], 'post':c[2]})
+    elif(cmd == 'EDIT_POST'):
+        requests.post(apiUrl + '/edit_post', json={'cmd':cmd, 'username':c[1], 'post_id':c[2], 'post':c[3]})
+    elif(cmd == 'DELETE_POST'):
+        requests.post(apiUrl + '/delete_post', json={'cmd':cmd, 'username':c[1], 'post_id':c[2]})
+    elif(cmd == 'COMMENT'):
+        requests.post(apiUrl + '/comment', json={'cmd':cmd, 'username':c[1], 'post_id':c[2], 'comment':c[3]})
+
+
+def readInputFile(fileName):
+    with open(fileName, "r") as f:
+        for line in f:
+            line = line.strip().rstrip()
+            createCommand(line)
+
+if __name__ =="__main__":
+    fileName = sys.argv[1]
+    start = time.perf_counter()
+    readInputFile(fileName)
+    stop = time.perf_counter()
+    print("time taken:", stop - start)
